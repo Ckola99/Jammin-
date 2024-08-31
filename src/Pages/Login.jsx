d3b5d3375e6b51f54fce2894d17a6bdb5872df63
Ckola99 <Christopherkola@gmail.com>
2024-08-31 14:27:53 +0200

    implemented my supabase client

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,24 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../features/userSlice";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useNavigate } from "react-router-dom";
+
+const spotifyApi = new SpotifyWebApi();
+
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
-export default Login
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	const handleLogin = async () => {
+		try {
+			const userInfo = await spotifyApi.getMe();
+			dispatch(login(userInfo));
+		} catch (error) {
+			console.error("Error logging in:", error);
+		}
+	};
+
+	return <div>Login</div>;
+};
+export default Login;
